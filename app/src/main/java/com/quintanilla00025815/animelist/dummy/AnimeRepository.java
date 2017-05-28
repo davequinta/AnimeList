5bb67214b8f4f861dc786ed22886747a22882461
DYuusha <cesarlopezlima1@gmail.com>
2017-05-27 18:53:02 -0600

    los hice por separado para que sean entendibles, demasiado codigo en una class cuesta entenderlo

---
+++ app/src/main/java/com/quintanilla00025815/animelist/dummy/AnimeRepository.java
@@ -0,0 +1,27 @@
+package com.quintanilla00025815.animelist.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AnimeRepository {
+    private ArrayList<Anime> rAnimes = new ArrayList<>();
+    private HashMap<Integer,Anime> pAnimes = new HashMap<>();
+
+
+    public AnimeRepository() {
+        saveAnime(new Anime(1,"Konosuba",2,"Comedia","Ecchi",12,2,"CTM"));
+
+    }
+/*LELOUCH ESTA VIVO F.César*/
+
+    private void saveAnime(Anime pAnime){pAnimes.put(pAnime.getIdAnime(),pAnime);
+    }
+
+    public List<Anime> getAnime(){
+        Map<Integer,Anime> map = new TreeMap<>(pAnimes);
+        return new ArrayList<>(map.values());
+    }
+}
